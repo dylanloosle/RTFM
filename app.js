dfbbcc0bfc0bd6a64f133d72d26fafc18ba548ef
Dylan Loosle <dylanloosle@gmail.com>
2015-08-13 20:57:20 -0600

    finished

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = angular.module('rtfmApp', ['firebase', 'ngRoute']);
+
+app.constant('fb', {
+    url: 'https://testingfir.firebaseio.com/'
+});
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/threads', {
+            templateUrl: '/Javascript/RTFM/threads/threads.html',
+            controller: 'threadsCtrl',
+            resolve: {
+                threadsRef: function(threadService){
+                   return threadService.getThreads();
+                }
+            }
+        })
+        .when('/threads/:threadId', {
+            templateUrl: '/Javascript/RTFM/threads/thread.html',
+            controller: 'threadCtrl',
+            resolve: {
+
+                threadRef: function (threadService, $route) {
+                    return threadService.getThread($route.current.params.threadId);
+
+
+                },
+                commentsRef: function (threadService, $route) {
+                    return threadService.getComments($route.current.params.threadId);
+                }
+
+            }
+        })
+        .otherwise({
+            redirectTo: '/threads'
+        })
+
+
+});
